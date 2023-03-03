a5d6a283e598904d86ce9af6ea4b48b54cbb36b3
rucklless <rucklless@gmail.com>
2023-03-03 17:44:20 +0400

    finish number one

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,25 @@
             event.stopPropagation();
             $(this).parent().siblings().removeClass('open');
             $(this).parent().toggleClass('open');
+            var container = $(this);
+
+
+            $(document).mouseup(function (e) {
+                if (container.has(e.target).length === 0){
+                    container.parent().removeClass('open');
+                    //container.hide();
+                }
+            });
+        });
+        $(".mobile_menu").simpleMobileMenu({
+            onMenuLoad: function(menu) {
+                console.log(menu)
+                console.log("menu loaded")
+            },
+            onMenuToggle: function(menu, opened) {
+                console.log(opened)
+            },
+            "menuStyle": "slide"
         });
     });
 })(jQuery);
